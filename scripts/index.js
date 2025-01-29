a4763571750f193d330baa953a49306d49f88842
mattspieg <36459606+mattspieg@users.noreply.github.com>
2025-01-29 22:25:45 +0000

    1

--- scripts/index.js
+++
@@ -1,2 +0,0 @@
-WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic"]  }});
-!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
